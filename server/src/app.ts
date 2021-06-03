bce65dde9152015f1b2039b30bebc06c98f781c6
thomas.ktx <thomas.ktx@kakaocorp.com>
2021-06-03 13:03:39 +0900

    chore: Add cors plugin

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import fastify from 'fastify'
+import cors from 'fastify-cors'
 import db from '@decorators/db'
 import apiRoute from '@routes/api'
 
@@ -6,6 +7,16 @@ const PORT = process.env.PORT || '3000'
 
 const server = fastify({ logger: true })
 
+server.register(cors, {
+  origin: (origin, callback) => {
+    if (!origin || /localhost/.test(origin)) {
+      callback(null, true)
+      return
+    }
+    // Generate an error on other origins, disabling access
+    callback(new Error('Not allowed'), false)
+  },
+})
 server.register(db)
 server.register(apiRoute, { prefix: '/api' })
 
